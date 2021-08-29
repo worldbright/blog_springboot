79889b361e648772ffca065125c6fef66b28ed81
worldbright <42114900+worldbright@users.noreply.github.com>
2021-08-30 03:03:42 +0900

    First Step
    add HomeController
    add SecurityConfig
    add home.html
    
    First Step of the project

---
+++ src/test/java/com/worldbright/springblog/SpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldbright.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
